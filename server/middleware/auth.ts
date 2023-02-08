8eeafd46c0109ba5a55340d3ade1641afd8ee060
Andres Meidla <andres.meidla@gmail.com>
2023-02-08 14:53:32 +0200

    removed jsonwebtoken dependency as it is not necessary for the repro

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,27 +1,16 @@
-import jwt from 'jsonwebtoken';
-
 export default defineEventHandler((event) => {
   const parsedUrl = new URL(event.node.req.url ?? '/', 'http://localhost');
   const pathname = parsedUrl.pathname;
   if (pathname.startsWith('/login')) {
-    console.log('Allow', pathname);
+    console.log(pathname, 'Allow');
     return;
   }
   const authCookie = getCookie(event, 'myToken');
   if (authCookie) {
-    try {
-      const decoded = jwt.verify(authCookie, useRuntimeConfig().jwtSecret);
-      if (decoded) {
-        console.log('Auth Success', pathname);
-        event.context.auth = decoded;
-        return;
-      }
-    } catch (e) {
-      console.warn('While verifying token:', e);
-      deleteCookie(event, 'myToken');
-    }
+    console.log(pathname, 'Cookie found, allow');
+    return;
   }
-  console.log('Redirecting to login', event.node.req.url);
+  console.log(pathname, 'Redirecting to login');
   // redirecting
   return sendRedirect(event, `/login?initial=${event.node.req.url}`, 307);
 });
